0dcdfafc4aefeb74a8c958b1e2ec277c78935f73
adelppi <adelppi.baig@gmail.com>
2023-06-14 14:14:54 +0900

    update

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,3 +1,41 @@
+<script>
+import axios from 'axios';
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+            items: [],
+        }
+    },
+    computed: {
+    },
+    methods: {
+        fetchTest() {
+            axios.get('http://192.168.3.2/previewData')
+                .then(response => {
+                    this.items = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            axios.get('https://adelppi.duckdns.org/previewData')
+                .then(response => {
+                    this.items = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    },
+    mounted() {
+        this.fetchTest()
+    }
+}
+</script>
+
 <template>
     <h1>ページ遷移できる</h1>
+    <h6>{{ items }}</h6>
 </template>
