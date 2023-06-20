655dce933c9d8f1acb2df0f32aa1fdd02d32ed77
adelppi <adelppi.baig@gmail.com>
2023-06-20 11:42:14 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Kousensai',
+  // base: '/Kousensai',
+  publicPath: './Kousensai',
   // build: {
   //   outDir: '/',
   // },
