83eb38f37eefc4e6815ad307d41a3d788c21bbc6
adelppi <adelppi.baig@gmail.com>
2023-07-31 18:51:04 +0900

    wip

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -6,8 +6,8 @@ export default {
                 { link: "/Home", label: "ホーム" },
                 { link: "/Test", label: "企画紹介" },
                 { link: "/Greet", label: "ごあいさつ" },
-                { link: "/Test", label: "パンフレット" },
-                { link: "/Test", label: "アクセス" }
+                { link: "/Test1", label: "パンフレット" },
+                { link: "/Test2", label: "アクセス" }
             ],
             currentPage: ""
         }
@@ -22,11 +22,9 @@ export default {
     <header class="menu-bar">
         <nav>
             <ul>
-                <li v-for="menu in menus">
+                <li v-for="menu in menus" class="menu" :class="{ 'underline': menu.link === currentPage }">
                     <RouterLink :to="menu.link">
-                        <div :class="{ 'underline': menu.link === currentPage }">
-                            {{ menu.label }}
-                        </div>
+                        {{ menu.label }}
                     </RouterLink>
                 </li>
             </ul>
@@ -36,23 +34,60 @@ export default {
 
 
 <style scoped>
-.underline {
-    animation: correct 0.5s ease-out 1;
+.menu {
+    padding: 1% 1%;
+    background-color: #ffffff;
+    transition: background-color 0.25s ease-in-out;
 }
 
-@keyframes underline {
-    0% {
-        border-bottom: 2px solid rgb(0, 119, 199);
-    }
-    100% {
-        border-bottom: 2px solid rgb(0, 119, 199);
-    }
+.menu:hover {
+    background-color: #ececec;
 }
+
+.underline {
+  position: relative;
+  display: inline-block;
+}
+
+.underline:before {
+    content: "";
+    position: absolute;
+    left: 50%;
+    bottom: 7.5%;
+    display: inline-block;
+    width: 75%;
+    height: 2px;
+    transform: translateX(-50%);
+    background-color: rgb(0, 119, 199);
+    border-radius: 2px;
+}
+/* .underline:hover {
+    content: "";
+    position: absolute;
+    left: 50%;
+    bottom: 7.5%;
+    display: inline-block;
+    width: 75%;
+    height: 2px;
+    transform: translateX(-50%);
+    background-color: rgb(0, 119, 199);
+    border-radius: 2px;
+} */
+
+/* 
+.underline {
+    border-bottom: 2px solid rgb(0, 119, 199);
+    transition: background-color 0.25s ease-in-out;
+}
+
+.underline:hover {
+    border-bottom: 2px solid rgb(0, 119, 199);
+} */
 
 .menu-bar {
     position: fixed;
     top: 0;
-    background-color: #d6d6d6;
+    background-color: #ffffff;
     color: #000000;
     padding: 10px;
     z-index: 100;
@@ -78,5 +113,4 @@ nav a {
 
 nav a:hover {
     color: rgb(0, 119, 199);
-}
-</style>
+}</style>
