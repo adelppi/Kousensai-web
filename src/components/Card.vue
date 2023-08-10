2b9a05a4dc171f7ec0b4fda5859eefc7ac94c99a
adelppi <adelppi.baig@gmail.com>
2023-08-10 17:05:13 +0900

    update

---
+++ src/components/Card.vue
@@ -0,0 +1,71 @@
+<script>
+export default {
+    data() {
+        return {
+            name: "企画名",
+            content: "企画概要"
+        }
+    },
+    created() {
+    }
+}
+</script>
+
+<template>
+    <div class="card card-skin">
+        <div class="card__imgframe"></div>
+        <div class="card__textbox">
+            <div class="card__titletext">
+                {{ name }}
+            </div>
+            <div class="card__overviewtext">
+                {{ content }}
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.card {
+    width: 288px;
+    height: auto;
+}
+
+.card-skin .card__textbox {
+    background: #333333;
+    color: #ffffff;
+}
+
+.card__imgframe {
+    width: 100%;
+    height: auto;
+    padding-top: 56.25%;
+    background: url(https://placehold.jp/640x360.png) no-repeat center;
+    background-size: cover;
+    box-sizing: border-box;
+}
+
+.card__textbox {
+    width: 100%;
+    height: auto;
+    padding: 20px 18px;
+    background: #ffffff;
+    box-sizing: border-box;
+}
+
+.card__textbox>*+* {
+    margin-top: 10px;
+}
+
+.card__titletext {
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 125%;
+}
+
+.card__overviewtext {
+    font-size: 12px;
+    line-height: 150%;
+}
+</style>
--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,8 +1,10 @@
 <script>
 import MenuBar from '../components/MenuBar.vue'
+import Card from '../components/Card.vue'
 export default {
     components: {
         MenuBar,
+        Card
     }
 }
 </script>
@@ -11,6 +13,8 @@ export default {
     <MenuBar />
     <h1>企画紹介</h1>
     <h2>企画一覧</h2>
+    <Card />
+
 </template>
 
 <style scoped>
