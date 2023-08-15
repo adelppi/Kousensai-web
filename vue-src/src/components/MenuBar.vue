283d0c4dd362fbbef41799113d3b98ec315cc4b0
adelppi <adelppi.baig@gmail.com>
2023-08-15 13:45:38 +0900

    wip

--- vue-src/src/components/MenuBar.vue
+++ vue-src/src/components/MenuBar.vue
@@ -3,11 +3,11 @@ export default {
     data() {
         return {
             menus: [
-                { path: "/Home", label: "ホーム" },
-                { path: "/Introduction", label: "企画紹介" },
-                { path: "/Greeting", label: "ごあいさつ" },
-                { path: "/Brochure", label: "パンフレット" },
-                { path: "/Access", label: "アクセス" }
+                { path: "/Home", label: "ホーム", icon: "home" },
+                { path: "/Greeting", label: "ごあいさつ", icon: "waving_hand" },
+                { path: "/Introduction", label: "企画紹介", icon: "storefront" },
+                { path: "/Brochure", label: "パンフレット", icon: "map" },
+                { path: "/Access", label: "アクセス", icon: "location_on" }
             ],
             currentPage: "",
             isMobile: false,
@@ -27,6 +27,8 @@ export default {
 </script>
 
 <template>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,300,0,0" />
     <div class="menu-bar">
         <div class="menu-bar-nav">
             <RouterLink to="/Home">
@@ -35,6 +37,9 @@ export default {
             <div class="spacer"></div>
             <RouterLink v-for="menu in menus" :to="menu.path" class="menu-item">
                 <div class="menu-link">
+                    <span class="material-symbols-outlined">
+                        {{ menu.icon }}
+                    </span>
                     <span class="link-text">{{ menu.label }}</span>
                 </div>
             </RouterLink>
@@ -45,8 +50,6 @@ export default {
 
 <style scoped>
 .menu-bar {
-    width: 15rem;
-    height: 100vh;
     position: fixed;
     background-color: #222;
 }
@@ -62,31 +65,57 @@ export default {
 }
 
 .menu-item {
-    width: 100%;
+    width: 62.5%;
+    padding: 0.5rem;
+    margin-bottom: 2rem;
+    color: #ffffff;
+}
+
+.menu-item:hover {
+    color: #222;
+    background-color: #ffffff;
+    transition: all 0.25s;
 }
 
 .menu-link {
     display: flex;
     align-items: center;
-    height: 5rem;
-    color: white;
     text-decoration: none;
 }
 
 .link-text {
     margin-left: auto;
     margin-right: auto;
-    color: #ffffff;
     text-decoration: inherit;
-}
-
-.link-text:hover {
-    color: #222;
-    background-color: #ffffff;
-    transition: all 0.25s;
 }
 
 .spacer {
     margin-bottom: 5rem;
+}
+
+/* モバイルのとき */
+@media only screen and (max-width: 600px) {
+    .menu-bar {
+        bottom: 0;
+        width: 100vw;
+        height: 5rem;
+    }
+
+    .menu-bar-nav {
+        flex-direction: row;
+    }
+
+    .menu-link {
+        justify-content: center;
+    }
+
+}
+
+/* モバイルでないとき */
+@media only screen and (min-width: 600px) {
+    .menu-bar {
+        width: 15rem;
+        height: 100vh;
+    }
 }
 </style>
