bc7d73f7ee37e39abf31d5c5a9c165f5a1210448
adelppi <adelppi.baig@gmail.com>
2023-08-26 08:22:11 +0900

    update & build

---
+++ frontend/src/views/Preparing.vue
@@ -0,0 +1,26 @@
+<template>
+    <body>
+        <main>
+            <div>
+                <img src="../assets/logo.svg">
+            </div>
+            <h1>高専祭公式サイト 只今準備中！</h1>
+        </main>
+    </body>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+img {
+    width: 10rem;
+}
+
+main {
+    margin: auto;
+    padding: 25%;
+}</style>
