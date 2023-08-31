f226e9ba6f2b739729539a3fbc70f7a9d10d69b9
adelppi <adelppi.baig@gmail.com>
2023-08-31 21:51:33 +0900

    build

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,33 +9,5 @@ export default {
 
 <template>
   <MenuBar v-if="$route.path != '/Preparing'" />
-  <router-view v-slot="{ Component }">
-    <transition name="v" mode=”out-in”>
-      <Component :is="Component" />
-    </transition>
-  </router-view>
+  <router-view />
 </template>
-
-<style scoped>
-.v-enter {
-  transform: translate(-100px, 0);
-  opacity: 0;
-}
-.v-enter-to {
-  opacity: 1;
-}
-.v-enter-active {
-  transition: all 1s 0s ease;
-}
-.v-leave {
-  transform: translate(0, 0);
-  opacity: 1;
-}
-.v-leave-to {
-  transform: translate(100px, 0);
-  opacity: 0;
-}
-.v-leave-active {
-  transition: all .5s 0s ease;
-}
-</style>
