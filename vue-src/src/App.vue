0fbd47a5d1b6beb612c1cd4983c7d0d675e17b56
AndreiTMCIT <m20245@g.metro-cit.ac.jp>
2023-09-07 17:24:25 +0900

    build?

---
+++ vue-src/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import MenuBar from './components/MenuBar.vue'
+export default {
+    components: {
+        MenuBar,
+    }
+}
+</script>
+
+<template>
+  <MenuBar />
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+</template>
