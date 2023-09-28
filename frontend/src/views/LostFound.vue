78dc3d8a1ceb174c6b6baefab31f643e77cec2d2
AndreiTMCIT <m20245@g.metro-cit.ac.jp>
2023-09-28 16:14:45 +0900

    added base for lostFound.vue

--- frontend/src/views/LostFound.vue
+++ frontend/src/views/LostFound.vue
@@ -1,9 +1,95 @@
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            lostItems: [
+                {
+                    name: 'something',
+                    place: 'somewhere',
+                    property: 'specialProperty'
+                },
+                {
+                    name: 'something',
+                    place: 'somewhere',
+                    property: 'specialProperty'
+                },
+                {
+                    name: 'something',
+                    place: 'somewhere',
+                    property: 'specialProperty'
+                },
+                {
+                    name: 'something',
+                    place: 'somewhere',
+                    property: 'specialProperty'
+                }
+            ],
+        }
+    },
+    methods: {
+        fetchLostItems() {
+            axios.get(import.meta.env.VITE_API_URL + '/getLostFoundItems')
+                .then(response => {
+                    this.plostItems = response.data
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    },
+    mounted() {
+        this.fetchLostItems();
+    }
+}
+</script>
+
 <template>
     <body>
         <main>
             <h1>落とし物画面</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>物品名</th>
+                        <th>見つかった場所</th>
+                        <th>特徴</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item) in lostItems" :key="item">
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.place }}</td>
+                        <td>{{ item.property }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </main>
     </body>
 </template>
 
-<style scoped></style>
+<style scoped>
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-bottom: 10px;
+}
+
+th {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: center;
+}
+
+td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+}
+
+th {
+    background-color: #f2f2f2;
+}
+
+</style>
