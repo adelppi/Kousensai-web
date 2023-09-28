d4d2abcdcae7af31731d47529c3becb5526074d0
AndreiTMCIT <m20245@g.metro-cit.ac.jp>
2023-09-28 15:52:43 +0900

    added Model.vue

---
+++ frontend/src/components/Module.vue
@@ -0,0 +1,71 @@
+<script>
+import axios from 'axios'
+
+export default {
+    props: {
+        id: Number,
+        vote: Number,
+        team_name: String,
+        project_name: String,
+        project_description: String,
+        project_space: String,
+        description: String,
+        imagePath: String,
+    },
+    data() {
+        return {
+            url: "",
+            isVoteButtonDisabled: false,
+            votedProjects: []
+        }
+    },
+    methods: {
+        incrementVote(id) {
+            this.getVotedProjects()
+            this.url = import.meta.env.VITE_API_URL + `/projects/${id}/increment-vote`
+            axios.post(this.url)
+            this.pushVariableToLocalStorage(id)
+            this.checkIfVoted()
+        },
+        decrementVote(id) {
+            this.getVotedProjects()
+            this.url = import.meta.env.VITE_API_URL + `/projects/${id}/decrement-vote`
+            axios.post(this.url)
+            this.deleteVariableFromLocalStorage(id)
+            this.checkIfVoted()
+        },
+        getVotedProjects() {
+            const storedData = localStorage.getItem('votedProjects');
+            if (storedData) {
+                this.votedProjects = JSON.parse(storedData);
+            }
+        },
+        pushVariableToLocalStorage(id) {
+            this.votedProjects.push(id)
+            localStorage.setItem('votedProjects', JSON.stringify(this.votedProjects));
+        },
+        deleteVariableFromLocalStorage(id) {
+            this.votedProjects = this.votedProjects.filter(item => item !== id);
+            localStorage.setItem('votedProjects', JSON.stringify(this.votedProjects));
+        },
+        checkIfVoted() {
+            if (this.votedProjects.includes(this.id)) {
+                this.isVoteButtonDisabled = true
+            } else {
+                this.isVoteButtonDisabled = false
+            }
+        },
+    },
+    mounted() {
+        this.getVotedProjects()
+        this.checkIfVoted()
+    }
+};
+</script>
+
+<template>
+</template>
+  
+<style scoped>
+</style>
+  
