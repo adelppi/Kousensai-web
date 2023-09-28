9513f904ee08ed2b9b4b030afc7c9b537b657895
adelppi <adelppi.baig@gmail.com>
2023-09-28 19:09:06 +0900

    bugfix: formatter

--- frontend/src/assets/thumbnailFormatter.py
+++ frontend/src/assets/thumbnailFormatter.py
@@ -4,7 +4,8 @@ import re
 import os
 
 for path in glob.glob("thumbnails/*"):
-    img = Image.open(path).convert('RGB')
-    number = max(re.findall(r"\d+", path))
-    img.save(f"thumbnails/{number}.png", "PNG")
-    os.remove(path)
+    if not "png" in path:
+        img = Image.open(path).convert('RGB')
+        number = max(re.findall(r"\d+", path))
+        img.save(f"thumbnails/{number}.png", "PNG")
+        os.remove(path)
