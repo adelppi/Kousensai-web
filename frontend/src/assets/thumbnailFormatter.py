f5cbee62f5257870201c5ba181e6f8d66e31b86d
ryoww <ryo.shimozawa.0121@gmail.com>
2023-09-30 22:23:24 +0900

    thumnails formatter update

--- frontend/src/assets/thumbnailFormatter.py
+++ frontend/src/assets/thumbnailFormatter.py
@@ -1,11 +1,19 @@
-from PIL import Image
+import cv2
 import glob
 import re
 import os
 
-for path in glob.glob("thumbnails/*"):
-    if not "png" in path:
-        img = Image.open(path).convert('RGB')
-        number = max(re.findall(r"\d+", path))
-        img.save(f"thumbnails/{number}.png", "PNG")
+for path in glob.glob("thumbails/*"):
+    img = cv2.imread(path)
+
+    number = max(re.findall(r"\d+", path))
+
+    if img.shape[0] > img.shape[1]:
+        img_resized = cv2.resize(img, (int(600/img.shape[0]*img.shape[1]), 600))
+        cv2.imwrite(f"outputs/{number}.png", img_resized)
+        os.remove(path)
+
+    else:
+        img_resized = cv2.resize(img, (600, int(600/img.shape[1]*img.shape[0])))
+        cv2.imwrite(f"outputs/{number}.png", img_resized)
         os.remove(path)
