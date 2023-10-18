a9f1e377442d4aeb97bcfabd8a291c84048c7931
adelppi <adelppi.baig@gmail.com>
2023-10-18 14:21:32 +0900

    clean

--- frontend/src/assets/thumbnails/高専祭_files/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "/node_modules/.vite/deps/vue.js?v=6ee544a3"
-import "/src/style.css"
-import App from "/src/App.vue"
-import router from "/src/router/index.js"
-
-createApp(App)
-    .use(router)
-    .mount('#app')
