fae9d13ecbafd6cfd25931d10447afee85a3aa7f
adelppi <adelppi.baig@gmail.com>
2023-10-24 11:10:02 +0900

    CSS完全に理解した

--- frontend/src/views/Brochure.vue
+++ frontend/src/views/Brochure.vue
@@ -2,27 +2,16 @@
     <body>
         <main>
             <div class="title">パンフレット</div>
-            <iframe src="https://drive.google.com/file/d/1B777DSBrMc1-i1xI1IW0JBXg-FA5eQPZ/preview"></iframe>
+            <div style="display: flex; width: 100%; justify-content: center;">
+                <iframe src="https://drive.google.com/file/d/1B777DSBrMc1-i1xI1IW0JBXg-FA5eQPZ/preview"></iframe>
+            </div>
         </main>
     </body>
 </template>
 
 <style scoped>
-main {
-    margin: 0rem 30%;
-    padding: 1rem;
-}
-
 iframe {
-    width: 100%;
+    width: 90%;
     height: 75vh;
-}
-
-@media only screen and (max-width: 800px) {
-
-    main {
-        margin: 0;
-        padding-top: 0;
-    }
 }
 </style>
