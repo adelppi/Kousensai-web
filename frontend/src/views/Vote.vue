a451581acc3e3ad2978390a7d441546905d51387
AndreiTMCIT <m20245@g.metro-cit.ac.jp>
2023-10-28 03:04:34 +0900

    fixed: Vote.vue

--- frontend/src/views/Vote.vue
+++ frontend/src/views/Vote.vue
@@ -1,27 +1,39 @@
 <script>
 import axios from 'axios'
 import Card from '../components/Card.vue'
+import Module from '../components/Module.vue'
 
 export default {
     components: {
-        Card
+        Card,
+        Module,
     },
     data() {
         return {
             extra: "",
-            topThreeProjects: []
+            topTenProjects: [],
+            shownId: {
+                type: Number,
+                default: null
+            },
+            moduleShown: false,
         }
     },
     methods: {
         fetchTopTenProjects() {
             axios.get(import.meta.env.VITE_API_URL + '/getTopTenProjects')
                 .then(response => {
-                    this.topThreeProjects = response.data
+                    this.topTenProjects = response.data
                 })
                 .catch(error => {
                     console.log(error);
                 });
-        }
+        },
+        showModule(Number) {
+            this.shownId = Number;
+            console.log(this.shownId)
+            this.moduleShown = true;
+        },
     },
     mounted() {
         this.fetchTopTenProjects()
@@ -36,19 +48,32 @@ export default {
         <main>
             <div class="title">人気企画TOP10</div>
             <div class="project-container">
-                <div v-for="(i, index) in topThreeProjects" :key="index">
-                    <Card class="card" :id="i.id" :vote="i.vote" :team_name="i.team_name" :project_name="i.project_name"
-                        :project_space="i.project_space" :project_description="i.project_description"
-                        :imagePath="`${extra}/assets/thumbnails/${i.id}.png`" />
+                <div v-for="(i, index) in topTenProjects" :key="index">
+                <Card class="card" :id="i.id" :vote="i.vote" :team_name="i.team_name" :project_name="i.project_name"
+                    :project_space="i.project_space" :project_description="i.project_description" :index="index"
+                    :imagePath="`${extra}/assets/thumbnails/${i.id}.png`" @card-selected="showModule"/>
                 </div>
             </div>
+            <Module v-if="moduleShown" @close-module-event="moduleShown = false"
+                :id="topTenProjects[shownId].id" :shownId="shownId" :vote="topTenProjects[shownId].vote"
+                :team_name="topTenProjects[shownId].team_name" :project_name="topTenProjects[shownId].project_name"
+                :project_space="topTenProjects[shownId].project_space"
+                :project_description="topTenProjects[shownId].project_description"
+                :imagePath="`${extra}/assets/thumbnails/${topTenProjects[shownId].id}.png`" />
         </main>
     </body>
 </template>
 
 <style scoped>
+
 .project-container {
+    margin-top: 2rem;
     display: flex;
-    justify-content: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;
+    gap: 1rem;
 }
+
 </style>
